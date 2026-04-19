96f5e5ab7d093801b39c1cc902329eae5368a82a
rahil <rahil.ikram67@gmail.com>
2026-04-19 06:07:53 +0400

    asdasdasd

--- src/App.tsx
+++ src/App.tsx
@@ -116,7 +116,7 @@ function App() {
         </Application>}
 
         {!(assetsReady && sceneProps) && (
-          <div className="absolute inset-0 flex items-center justify-center">
+          <div className="flex items-center justify-center">
             <div className="text-white text-2xl font-bold">Loading...</div>
           </div>
         )}
